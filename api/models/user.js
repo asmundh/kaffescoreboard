a330ac4fb6ed739e46c8f42199acdd03f5c87b2c
asmundh <aasmund.haugse@gmail.com>
2020-10-05 11:59:25 +0200

    Remove .venv from github

--- api/models/user.js
+++ api/models/user.js
@@ -8,11 +8,10 @@ const userCheck = (query, checker) => !Joi.validate(query, checker).error;
 const user = Joi.object().keys({
   name: Joi.string().min(2).max(35).required(),
   study: Joi.string().valid('Komtek', 'Data').required(),
-  rfid: Joi.string().min(0).required(),
+  rfid: Joi.string().length(8).required(),
   kaffeScore: Joi.number().min(0).required(),
   year: Joi.number().min(1).max(6).required(),
   lastScan: Joi.date(),
 });
-
 
 export { userCheck, user };
